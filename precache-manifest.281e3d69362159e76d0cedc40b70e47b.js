d3e6bdb9ff25b52c620b52f1311933d160e220c1
Arjun Adobe <51747982+arjunadobe@users.noreply.github.com>
2020-07-20 03:12:52 +0530

    Updates

---
+++ precache-manifest.281e3d69362159e76d0cedc40b70e47b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbe521a9b334474326b582af378442b",
+    "url": "/cpoc1/index.html"
+  },
+  {
+    "revision": "06150c12759b93075a6f",
+    "url": "/cpoc1/static/css/main.38b15592.chunk.css"
+  },
+  {
+    "revision": "4defc9c527acad6d5c11",
+    "url": "/cpoc1/static/js/2.4326f175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cpoc1/static/js/2.4326f175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06150c12759b93075a6f",
+    "url": "/cpoc1/static/js/main.edb4a141.chunk.js"
+  },
+  {
+    "revision": "a5c22c4ec43542f63964",
+    "url": "/cpoc1/static/js/runtime-main.1b968eb7.js"
+  }
+]);
